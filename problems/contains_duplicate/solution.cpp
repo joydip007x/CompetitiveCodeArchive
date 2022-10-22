9d747048013b5b5bf422dae36dc5001bbee62dcf
Joydip Das <joydip007x@gmail.com>
2022-10-22 20:22:58 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

--- problems/contains_duplicate/solution.cpp
+++ problems/contains_duplicate/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        	set<long long>s;
+        	unordered_set<long long>s;
             for(auto i:nums){
                 if(s.find(i)!=s.end()) return true;
                 s.insert(i);
