7a95a04ba045c09048a970ab676f69dc1f0bbf8f
Joydip Das <joydip007x@gmail.com>
2023-04-29 20:54:38 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- problems/palindrome_number/solution.cpp
+++ problems/palindrome_number/solution.cpp
@@ -4,11 +4,9 @@ public:
        
        if(x<=0) return (x==0);
        double exp= log10(x);
-       int exp_int=(int)exp;
-       int len=(exp_int+1)/2;
-
+       int len=((int)exp+1)/2;
        for(int i=0; i<len; i++,x=x/10)
-           if( (int)pow(10, double(exp-exp_int+i)) %10 != x%10 ) 
+           if( (int)pow(10, double(exp-(int)exp+i)) %10 != x%10 ) 
                  return false;
        return true;
     }
