b3ef9c2765e289599ba7edf233702f6c6a3df495
Joydip Das <joydip007x@gmail.com>
2023-05-01 09:44:04 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- problems/palindrome_number/solution.cpp
+++ problems/palindrome_number/solution.cpp
@@ -8,7 +8,7 @@ public:
        for(int i=0; i<len; i++,x=x/10)
            if( (int)pow(10, double(exp-(int)exp+i)) %10 != x%10 ) 
                  return false;
-                 
+                 //
        return true;
     }
 };
