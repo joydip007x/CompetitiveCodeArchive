88e5c1d25c373057a0880547ccee3f60ee183751
Joydip Das <joydip007x@gmail.com>
2023-05-05 05:03:06 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+        
+       if(s.length()!= t.length()) return false;
+
+       unordered_map<int,int>u;
+       for(int i=0; i<s.length(); i++){
+           u[s[i]]++;  u[t[i]]--;
+       }
+       for(auto i:u) 
+            if(i.second) return false;
+       return true;
     }
 };
