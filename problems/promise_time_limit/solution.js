9551d42f6b2a802d33ee915aee4433ca4da0dd55
Joydip Das <joydip007x@gmail.com>
2023-05-16 04:55:49 +0000

    Sync LeetCode submission - Promise Time Limit (javascript)

---
+++ problems/promise_time_limit/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        return new Promise((resolve,reject)=>{
+
+            const timeOut= setTimeout(()=>{
+                reject("Time Limit Exceeded")
+            },t);
+
+            fn(...args)
+                .then(resolve)
+                .catch(reject)
+                .finally(()=>clearTimeout(timeOut))
+
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
