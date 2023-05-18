fe56fa13aa3e8f51bd9c38fac9005c417cc1ff8c
Joydip Das <joydip007x@gmail.com>
2023-05-18 05:18:49 +0000

    Sync LeetCode submission - Cache With Time Limit (javascript)

---
+++ problems/cache_with_time_limit/solution.js
@@ -0,0 +1,50 @@
+var TimeLimitedCache = function() {
+    this.cache= {};
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    if(this.cache[key] && this.cache[key].timer){
+        clearTimeout(this.cache[key].timer);
+        this.cache[key].value = value;
+        this.cache[key].timer = setTimeout(()=>{
+           delete this.cache[key]
+        },duration);
+        return true;
+    }
+    else{
+        this.cache[key]={
+            value:value,
+            timer:setTimeout(()=>{delete this.cache[key] },duration)
+        }
+        return false;
+    }
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return (this.cache[key])?  this.cache[key].value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return Object.keys(this.cache).length;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
