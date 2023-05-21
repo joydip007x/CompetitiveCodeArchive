24c916c97d5ff84cbd9838927313299c607d24ac
Joydip Das <joydip007x@gmail.com>
2023-05-21 19:33:01 +0000

    Sync LeetCode submission - JSON Deep Equal (javascript)

--- problems/json_deep_equal/solution.js
+++ problems/json_deep_equal/solution.js
@@ -4,29 +4,34 @@
  * @return {boolean}
  */
 var areDeeplyEqual = function(o1, o2) {
-    if (o1 === o2) return true;
-    if (o1 === null || o2 === null) return false;
-    if (String(o1) !== String(o2)) return false;
 
-    if (typeof o1 !== 'object') {
-        return o1 === o2;
-    }
+    var objects= [[ o1, o2 ]];
 
-    if (Array.isArray(o1)) {
-        if (o1.length !== o2.length) return false;
+    while(objects.length){
 
-        for (let i = 0; i < o1.length; i++) {
-            if (!areDeeplyEqual(o1[i], o2[i])) return false;
+        [o1,o2]=objects.pop();
+
+      //  console.log('o-',o1,o2)
+        if(o1===o2)continue;
+        if(typeof o1 !=='object' || typeof o2!=='object') return false;
+        if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+
+        const keys1=Object.keys(o1);
+        const keys2=Object.keys(o2);
+
+        // console.log(keys1,':',keys2);
+        // console.log(keys1.length,'L:L',keys2.length);
+
+        if(keys1.length !== keys2.length) return false;
+
+        for(const key of keys1){
+           // console.log(key,'::');
+            if(!(key in o2)) return false;
+            objects.push([  o1[key] , o2[key]  ]);
         }
-
-        return true;
-    }
-
-    if (Object.keys(o1).length !== Object.keys(o2).length) return false;
-
-    for (const key in o1) {
-        if (!areDeeplyEqual(o1[key], o2[key])) return false;
-    }
-
+     //   console.log('H')
+      
+    }   
     return true;
+
 };
