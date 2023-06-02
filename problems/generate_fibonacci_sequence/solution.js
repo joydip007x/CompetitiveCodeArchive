43cbb92674c46344f3bd3c8f61e0e86e70d5b4bd
Joydip Das <joydip007x@gmail.com>
2023-06-02 05:45:23 +0000

    Sync LeetCode submission - Generate Fibonacci Sequence (javascript)

---
+++ problems/generate_fibonacci_sequence/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*(a=0,b=1) {
+    yield a;
+    yield* fibGenerator(b,a+b);
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
