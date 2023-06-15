10a570ec93c3df2a6f4d3f6572c51b5af6b17d39
Joydip Das <joydip007x@gmail.com>
2023-06-15 10:56:41 +0000

    Sync LeetCode submission - Same Tree (cpp)

--- LeetCode/problems/same_tree/solution.cpp
+++ LeetCode/problems/same_tree/solution.cpp
@@ -12,14 +12,16 @@
 class Solution {
 public:
     string s1="",s2="";
-    string traverseTree(TreeNode* root,string s,string ex){
-        if(root==NULL){return " ";}
-        s+=traverseTree(root->left,"","L");
-        s+=to_string(root->val)+ex;
-        s+=traverseTree(root->right,"","R");
-        return s;
+    void traverseTree(TreeNode* root,string* s,string ex){
+        if(root==NULL){return;}
+        traverseTree(root->left,s,"L");
+        s->append(to_string(root->val)+ex);
+        traverseTree(root->right,s,"R");
     }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return traverseTree(p,"","R")==traverseTree(q,"","R");
+
+        traverseTree(p,&s1,"M");
+        traverseTree(q,&s2,"M");
+        return s1==s2;
     }
 };
