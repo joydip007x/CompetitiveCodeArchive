7e94782932aac39e60eb787f1d9335a2673f4070
Joydip Das <joydip007x@gmail.com>
2023-06-24 20:38:58 +0000

    Sync LeetCode submission - Pascal's Triangle II (cpp)

--- LeetCode/problems/pascal's_triangle_ii/solution.cpp
+++ LeetCode/problems/pascal's_triangle_ii/solution.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>>pascal;
-    bool create(int i){
-
-        if(i<=1 || pascal[i].size() ) return true;
-        create(i-1);
-        vector<int>tmp=pascal[i-1];
-        vector<int>tmp2(i,1);
-        for(int j=1;j<i-1;j++)tmp2[j]=tmp[j]+tmp[j-1];
-        pascal[i]=tmp2;
-        return true;
-    }
     vector<int> getRow(int rowIndex) {
-        pascal.resize(35);
-        pascal[1]=vector<int>{1};
-        create(rowIndex+1);
-        return pascal[rowIndex+1];
+        vector<int> row(rowIndex + 1, 1);  
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = i - 1; j > 0; --j) {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+        return row;
     }
 };
