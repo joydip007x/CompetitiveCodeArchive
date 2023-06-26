f94c9c93800c7ed8139a05dec3d7b2dbc45d71b2
Joydip Das <joydip007x@gmail.com>
2023-06-26 10:58:52 +0000

    Sync LeetCode submission - Single Number (cpp)

--- LeetCode/problems/single_number/solution.cpp
+++ LeetCode/problems/single_number/solution.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>u;
         int ans=0;
-        for(auto i:nums)u[i]++; 
-        for(auto i:u){if(i.second==1)ans=i.first;}
+        for(auto i:nums)ans^=i;
         return ans;
         
     }
