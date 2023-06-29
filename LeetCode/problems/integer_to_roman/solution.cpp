75e586e693b9b8a4fd9101293dfc0e6e740ac690
Joydip Das <joydip007x@gmail.com>
2023-06-29 09:26:23 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

--- LeetCode/problems/integer_to_roman/solution.cpp
+++ LeetCode/problems/integer_to_roman/solution.cpp
@@ -5,26 +5,29 @@ public:
         {9,"IX"},{40,"XL"},{90,"XC"},{400,"CD"},{900,"CM"},{1,"I"},{5,"V"},{10,"X"},
         {50,"L"},{100,"C"},{500,"D"},{1000,"M"}};
 
-    string getExp(int x){
+    string getExp(int x,int base){
            
        string ret2="";
-       int base=(int)pow(10,(int)log10(x)),cnt=0;
+       int cnt=0; x=x*base;
        while(!u[x].length()){x=x-base; ret2+=u[base];}
        return u[x]+ret2;
         
     }
     string intToRoman(int num) {
-        string ans="";
-       
-        int len=(int)log10(num)+1,pre=0;
-        for(int i=0; i<len ; i++){
-            int y=num-(num% (int)pow(10,len-i-1) )-pre;
-            pre+=y;
+        string ans="",n2="";
+        int c=1;
+        while(num){
+            n2+=char(num%10);
+            num=num/10;
+            c=c*10;
+        }
+        reverse(n2.begin(),n2.end()); c=c/10;
+  
+        for(int i=0; i<n2.length() ; i++,c=c/10){
+            int y=n2[i]*c;
+            if(!y)continue;
             if(u[y].length()) ans+=u[y];
-            else{
-                if(!y)continue;
-                ans+=getExp(y);
-            }
+            else{  ans+=getExp(int(n2[i]),c);  }
         }
         return ans;
     }
