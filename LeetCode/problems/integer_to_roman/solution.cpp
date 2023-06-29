a0cb4987ca51be18dc6ae9c4208a5e84edce3f71
Joydip Das <joydip007x@gmail.com>
2023-06-29 09:30:02 +0000

    Sync LeetCode submission - Integer to Roman (cpp)

--- LeetCode/problems/integer_to_roman/solution.cpp
+++ LeetCode/problems/integer_to_roman/solution.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-
-    unordered_map<int,string>u={{4,"IV"},
-        {9,"IX"},{40,"XL"},{90,"XC"},{400,"CD"},{900,"CM"},{1,"I"},{5,"V"},{10,"X"},
-        {50,"L"},{100,"C"},{500,"D"},{1000,"M"}};
-
-    string getExp(int x,int base){
-           
-       string ret2="";
-       int cnt=0; x=x*base;
-       while(!u[x].length()){x=x-base; ret2+=u[base];}
-       return u[x]+ret2;
-        
-    }
     string intToRoman(int num) {
-        string ans="",n2="";
-        int c=1;
-        while(num){
-            n2+=char(num%10);
-            num=num/10;
-            c=c*10;
+        string roman="";
+        vector<pair<int, string>> stor= {{1000 ,"M"},{900,"CM"}, {500 ,"D"}, {400, "CD"},{100,"C"},{90,"XC"},
+        {50 ,"L"},{40 ,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+        
+        for(int i =0 ; i<stor.size(); i++){
+            while(num>=stor[i].first){
+                 roman+=stor[i].second;
+                 num-=stor[i].first;
+            }
+         
+
         }
-        reverse(n2.begin(),n2.end()); c=c/10;
-  
-        for(int i=0; i<n2.length() ; i++,c=c/10){
-            int y=n2[i]*c;
-            if(!y)continue;
-            if(u[y].length()) ans+=u[y];
-            else{  ans+=getExp(int(n2[i]),c);  }
-        }
-        return ans;
+        return roman;
+        
     }
 };
