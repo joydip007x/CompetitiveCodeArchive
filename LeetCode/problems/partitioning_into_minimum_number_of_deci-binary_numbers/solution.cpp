3393833d9cfd9ae4ba41ea45fffd0ed50ce306b2
Joydip Das <joydip007x@gmail.com>
2023-06-30 10:28:30 +0000

    Sync LeetCode submission - Partitioning Into Minimum Number Of Deci-Binary Numbers (cpp)

--- LeetCode/problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.cpp
+++ LeetCode/problems/partitioning_into_minimum_number_of_deci-binary_numbers/solution.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+
     int minPartitions(string n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
         int x=0;
+
         for(const auto& i:n)x=max(x,i-'0');
         return x;
     }
