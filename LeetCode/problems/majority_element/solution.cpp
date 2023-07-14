d03121cafac9a2849b2ce0ea632aa9c21acd4675
Joydip Das <joydip007x@gmail.com>
2023-07-14 12:17:20 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- LeetCode/problems/majority_element/solution.cpp
+++ LeetCode/problems/majority_element/solution.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>u;
-        int mx=0;
-        for(const auto& i:nums){
-            u[i]++;
-            if(u[i]>u[mx])mx=i;
-        }
-        return mx;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n/2];
     }
 };
