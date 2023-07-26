4d69b51c077097f75b886b8afbc9074515b29cb6
Joydip Das <joydip007x@gmail.com>
2023-07-26 09:19:08 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- LeetCode/problems/isomorphic_strings/solution.cpp
+++ LeetCode/problems/isomorphic_strings/solution.cpp
@@ -7,8 +7,8 @@ public:
         int ind1=0,ind2=0;
         for(int i=0;i<s.length();i++){
 
-             if( ( u1.find(s[i])!=u1.end() && u1[s[i]]!=t[i] )
-              ||  ( u2.find(t[i])!=u2.end() && u2[t[i]]!=s[i] ) ) 
+             if( ( u1[s[i]] && u1[s[i]]!=t[i] )
+              || ( u2[t[i]] && u2[t[i]]!=s[i] ) ) 
               return false;
     
              u1[s[i]] =t[i];
