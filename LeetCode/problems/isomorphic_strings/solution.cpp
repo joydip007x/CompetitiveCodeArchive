483f27e1bb27a8a32759a65d92e8abac021cbc4a
Joydip Das <joydip007x@gmail.com>
2023-07-26 09:17:46 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- LeetCode/problems/isomorphic_strings/solution.cpp
+++ LeetCode/problems/isomorphic_strings/solution.cpp
@@ -4,15 +4,10 @@ public:
 
         if(s.length()!= t.length() ) return false;
         unordered_map<int,int>u1,u2;
-        s=' '+s;
-        t=' '+t;
         int ind1=0,ind2=0;
-        for(int i=1;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
 
-             if(s[i]!=s[i-1]) ind1++;
-             if(t[i]!=t[i-1]) ind2++;
-             if(ind1!=ind2 
-              ||  ( u1.find(s[i])!=u1.end() && u1[s[i]]!=t[i] )
+             if( ( u1.find(s[i])!=u1.end() && u1[s[i]]!=t[i] )
               ||  ( u2.find(t[i])!=u2.end() && u2[t[i]]!=s[i] ) ) 
               return false;
     
