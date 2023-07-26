03ce4444b8ef2c07fad7110ca57406c1f87cd297
Joydip Das <joydip007x@gmail.com>
2023-07-26 10:33:50 +0000

    Sync LeetCode submission - Isomorphic Strings (cpp)

--- LeetCode/problems/isomorphic_strings/solution.cpp
+++ LeetCode/problems/isomorphic_strings/solution.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+      int hash[256] = {0};
+        bool istCharsMapped[256] = {0};
 
-        if(s.length()!= t.length() ) return false;
-        unordered_map<short int,short int>u1,u2;
-        
-        for(int i=0;i<s.length();i++){
-
-             if( ( u1[s[i]] && u1[s[i]]!=t[i] ) || ( u2[t[i]] && u2[t[i]]!=s[i] ) ) 
-              return false;
-    
-             u1[s[i]] =t[i];
-             u2[t[i]] =s[i];
+        for(int i=0; i<s.size(); i++){
+            if(hash[s[i]]==0 && istCharsMapped[t[i]]==0){
+                hash[s[i]] = t[i];
+                istCharsMapped[t[i]] =true;
+            }
+        }
+        for(int i=0;i<s.size(); i++){
+            if(char(hash[s[i]]) !=t[i]){
+                return false;
+            }
         }
         return true;
+
     }
 };
