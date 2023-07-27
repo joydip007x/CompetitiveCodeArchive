b5905a6595f7929b1bef7e6051cc9161e8b1d6fc
Joydip Das <joydip007x@gmail.com>
2023-07-27 19:37:44 +0000

    Sync LeetCode submission - Contains Duplicate II (cpp)

--- LeetCode/problems/contains_duplicate_ii/solution.cpp
+++ LeetCode/problems/contains_duplicate_ii/solution.cpp
@@ -1,28 +1,23 @@
-class Solution {
-public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+class Solution
+{
+  public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
         ios_base::sync_with_stdio(0);
         cin.tie(NULL);
         cout.tie(NULL);
-        
-        map<int,vector<int>>m;
-        int len=nums.size();
-        for(int i=0;i<len; i++) m[nums[i]].push_back(i);
-        
-        for(const auto& i:m ){
-           // cout<<" i.first "<<i.first<<" :: ";
-            vector<int>v=i.second;
-          //  for(auto h:v)cout<<h<<",";cout<<endl;
-            int len=v.size();
-            for(int hh=0; hh<len; hh++){
-              auto  g= upper_bound(v.begin(),v.end(),v[hh]+k);
-              g=prev(g);
-              if( *g!=v[hh] && abs(*g-v[hh])<=k) return true;
-            }
-          
-            //cout<<*(prev(g))<<" :\n";
-
+        const int n = nums.size();
+        vector <pair<int, int>> numsP;
+        for (int i = 0; i < n; ++i) numsP.emplace_back(nums[i], i);
+        try
+        {
+            sort(numsP.begin(), numsP.end(), [&] (const auto &l, const auto &r)
+            {
+                if (l.first == r.first && abs(l.second - r.second) <= k) throw exception();
+                else return (l.first == r.first) ? (l.second < r.second) : (l.first < r.first);
+            } );
         }
+        catch (...) { return true; }
         return false;
     }
 };
